bcef62cb1c3e27e6b5c92bc00a0d9738e186c2ba
hungnv <hungnv234@gmail.com>
2019-03-19 00:43:47 +0700

    update code

--- resources/views/vendor/mail/markdown/message.blade.php
+++ resources/views/vendor/mail/markdown/message.blade.php
@@ -1,11 +1,4 @@
 @component('mail::layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-            {{ config('app.name') }}
-        @endcomponent
-    @endslot
-
     {{-- Body --}}
     {{ $slot }}
 
@@ -17,11 +10,4 @@
             @endcomponent
         @endslot
     @endisset
-
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
-        @endcomponent
-    @endslot
 @endcomponent
