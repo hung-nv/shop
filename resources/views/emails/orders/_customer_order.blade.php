3abb054b6d566046b9a4ae38c3536714490922d0
hungnv <hungnv234@gmail.com>
2019-03-19 00:38:03 +0700

    update code

--- resources/views/emails/orders/_customer_order.blade.php
+++ resources/views/emails/orders/_customer_order.blade.php
@@ -2,30 +2,28 @@
     @component('mail::panel')
         Thông tin khách hàng
     @endcomponent
-    Tên: {{ $order->last_name }} <br/>
-    SĐT: {{ $order->telephone }} <br/>
-    Địa chỉ: {{ $order->address }} <br/>
-    Ghi chú: {{ $order->note }} <br/>
-    Tình trạng: Đơn hàng mới <br/>
-    Thời gian đặt hàng: {{ $order->created_at }} <br/>
 
-    @component('mail::panel')
-        Chi tiết đơn hàng
-    @endcomponent
+    Tên: {{ $order->last_name }}
+    SĐT: {{ $order->telephone }}
+    Địa chỉ: {{ $order->address }}
+    Ghi chú: {{ $order->note }}
+    Tình trạng: Đơn hàng mới
+    Thời gian đặt hàng: {{ $order->created_at }}
+
+
+    ###Chi tiết đơn hàng
 
     @component('mail::table')
-        | Tên sản phẩm       | Số lượng         | Đơn giá  | Thành tiền |
-        | ------------- |:-------------:| -------------:| ----------------:|
+        | Tên sản phẩm | Số lượng | Đơn giá | Thành tiền |
+        | ------------ | -------- | ------- | ---------- |
         @foreach($order->orderProducts as $product)
-            | {{ $product->name }}      | 1     | {{ number_format($product->price) }}      | {{ number_format($product->price * 1) }} VND |
+            | {{ $product->name }} | 1 | {{ number_format($product->price) }} | {{ number_format($product->price * 1) }} VND |
         @endforeach
     @endcomponent
 
-    Tiền trước KM: {{ number_format($order->total_money) }}VNĐ <br/>
-    Mã Giảm giá:  {{ $order->coupon_code }} ({{ $order->coupon_code_value }} %) <br/>
+    Tiền trước KM: {{ number_format($order->total_money) }}VNĐ
+    Mã Giảm giá:  {{ $order->coupon_code }} ({{ $order->coupon_code_value }} %)
     Tiền phải trả: {{ number_format($order->total_money - ($order->total_money * $order->coupon_code_value)/100) }} VNĐ
 
-    Thanks,<br>
-    {{ config('app.name') }}
-
+    Thanks,
 @endcomponent
