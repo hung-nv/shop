835c8ddb5ce80cb1a26f79de2c9e228b20ca05e8
hungnv <hungnv234@gmail.com>
2019-03-19 01:18:00 +0700

    update code

--- resources/views/emails/orders/_customer_order.blade.php
+++ resources/views/emails/orders/_customer_order.blade.php
@@ -1,4 +1,6 @@
-@component('mail::panel')
+@component('mail:message')
+    # Đơn hàng
+
     Thông tin khách hàng
 
     Tên: {{ $order->last_name }}
@@ -7,25 +9,18 @@
     Ghi chú: {{ $order->note }}
     Tình trạng: Đơn hàng mới
     Thời gian đặt hàng: {{ $order->created_at }}
-@endcomponent
 
-<br>
-<br/>
+    @component('mail::table')
+        | Tên sản phẩm | Số lượng | Đơn giá | Thành tiền |
+        | ------------ | -------- | ------- | ---------- |
+        @foreach($order->orderProducts as $product)
+            | {{ $product->name }} | 1 | {{ number_format($product->price) }} | {{ number_format($product->price * 1) }} VND |
+        @endforeach
+    @endcomponent
 
-@component('mail::table')
-    | Tên sản phẩm | Số lượng | Đơn giá | Thành tiền |
-    | ------------ | -------- | ------- | ---------- |
-    @foreach($order->orderProducts as $product)
-        | {{ $product->name }} | 1 | {{ number_format($product->price) }} | {{ number_format($product->price * 1) }} VND |
-    @endforeach
-@endcomponent
-
-<br>
-<br/>
-
-<p>
     Tiền trước KM: {{ number_format($order->total_money) }}VNĐ
     Mã Giảm giá:  {{ $order->coupon_code }} ({{ $order->coupon_code_value }} %)
     Tiền phải trả: {{ number_format($order->total_money - ($order->total_money * $order->coupon_code_value)/100) }} VNĐ
+
     Thanks,
-</p>
+@endcomponent
